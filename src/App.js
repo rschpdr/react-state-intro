026e7647da06b5cb98acefbba0d347da4acf9c8c
rschpdr <pedro.resch@gmail.com>
2020-09-29 14:21:06 -0300

    done with classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+
+  // Executa essa funçāo assim que esse componente for renderizado na tela
+  componentDidMount() {
+    this.updateState();
+  }
+
+  // Metodo pra atualizar o state
+  updateState() {
+    // A cada 1 segundo, atualiza o state para o horario atual
+    setInterval(() => {
+      // So podemos atualizar state no React atraves da funcao setState
+      this.setState({
+        hours: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+        seconds: new Date().getSeconds(),
+      });
+    }, 1000);
+  }
+
+  // Metodo responsavel por retornar o JSX do componente
+  render() {
+    return (
+      <div className="container">
+        <h1> Are you React-ing Ironhackers?</h1>
+        <h3>
+          Time now is:
+          {" " + this.state.hours}:{this.state.minutes}:{this.state.seconds}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
+// module.exports = App;
